a5680d1bfc090361723af8ec6d40e45e43128f6e
weberthRosa <62699317+weberthRosa@users.noreply.github.com>
2021-01-11 11:06:07 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<ul>
+  <li><a href="home.php">Home</a></li>
+  <li><a href="recentes.php">Recentes</a></li>
+  <li><a href="about.php">About</a></li>
+  <link rel="stylesheet" href="index.css" >
+  <li><a href="login.php">Login</a></li>
+  <li><a href="about.php">sing in</a></li>
+</ul>
+
+
+</head>
+<body>
+
+
+
+</body>
+</html>
